18d88b81a490b8555e5f1b35dd4f05d69bf637f9
learningmobile2 <35796679+learningmobile2@users.noreply.github.com>
2018-01-25 15:41:34 +0530

    initialsetup

---
+++ Code/SharedLib/SharedLibrary/SMS.Entities/IEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SMS.Entities
+{
+    interface IEntity
+    {
+        long Id { get; set; }
+        long LastUpdateId { get; set; }
+        string Name { get; set; }
+        string Description { get; set; }
+        string Note { get; set; }
+    }
+}
